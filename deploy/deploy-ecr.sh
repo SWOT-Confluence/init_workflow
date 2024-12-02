5d2db5a20ad66bae8f252f461b4780fdf034d6a7
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-12-02 22:06:21 +0000

    Enable local build and push of docker container image

--- deploy/deploy-ecr.sh
+++ deploy/deploy-ecr.sh
@@ -5,11 +5,13 @@
 # Command line arguments:
 # [1] registry: Registry URI
 # [2] repository: Name of repository to create
+# [3] local: Whether script is being run locally
 # 
 # Example usage: ./delpoy-ecr.sh "account-id.dkr.ecr.region.amazonaws.com" "docker-container-image"
 
 REGISTRY=$1
 REPOSITORY=$2
+IS_LOCAL=$3
 
 # Determine if repo exists
 response=$(aws ecr describe-repositories --repository-names "$REPOSITORY" 2>&1)
@@ -36,4 +38,17 @@ else
     else
         echo "Respository could not be created."
     fi
+fi
+
+if [[ "$IS_LOCAL" == "true" ]]; then
+    # Login
+    docker login -u AWS https://$REGISTRY -p $(aws ecr get-login-password --region us-west-2)
+
+    # Build
+    cd ..
+    docker build -t $REGISTRY/$REPOSITORY .
+
+    # Push
+    docker push $REGISTRY/$REPOSITORY
+    cd deploy
 fi
