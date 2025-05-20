37e144f9cc751d13ae943892c4bc0e87ab214873
Josh Garde <joshgarde@gmail.com>
2025-05-20 05:37:01 -0700

    Parameterize Terraform Child Module (#4)
    * Initial parameterization refactor
    
    * Lint
    
    * Lint
    
    * default_tags padding
    
    * Fix references to EFS and IAM roles in module
    
    ---------
    
    Co-authored-by: Nikki <17799906+nikki-t@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,46 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_efs_file_system" "input" {
+  creation_token = "${var.prefix}-input"
+}
+
+data "aws_efs_file_system" "flpe" {
+  creation_token = "${var.prefix}-flpe"
+}
+
+data "aws_efs_file_system" "moi" {
+  creation_token = "${var.prefix}-moi"
+}
+
+data "aws_efs_file_system" "diagnostics" {
+  creation_token = "${var.prefix}-diagnostics"
+}
+
+data "aws_efs_file_system" "offline" {
+  creation_token = "${var.prefix}-offline"
+}
+
+data "aws_efs_file_system" "validation" {
+  creation_token = "${var.prefix}-validation"
+}
+
+data "aws_efs_file_system" "output" {
+  creation_token = "${var.prefix}-output"
+}
+
+data "aws_efs_file_system" "logs" {
+  creation_token = "${var.prefix}-logs"
+}
+
+data "aws_iam_role" "job_role" {
+  name = "${var.prefix}-batch-job-role"
+}
+
+data "aws_iam_role" "exe_role" {
+  name = "${var.prefix}-ecs-exe-task-role"
+}
+
 locals {
   account_id = sensitive(data.aws_caller_identity.current.account_id)
   default_tags = length(var.default_tags) == 0 ? {
@@ -29,10 +69,22 @@ locals {
 }
 
 module "confluence-init-workflow" {
-  source            = "./modules/init"
-  app_name          = var.app_name
-  app_version       = var.app_version
-  aws_region        = var.aws_region
-  environment       = var.environment
-  prefix            = var.prefix
+  source = "./modules/init"
+  app_name = var.app_name
+  app_version = var.app_version
+  aws_region = var.aws_region
+  efs_file_system_ids = {
+    input = data.aws_efs_file_system.input.file_system_id
+    flpe = data.aws_efs_file_system.flpe.file_system_id
+    moi = data.aws_efs_file_system.moi.file_system_id
+    diagnostics = data.aws_efs_file_system.diagnostics.file_system_id
+    offline = data.aws_efs_file_system.offline.file_system_id
+    logs = data.aws_efs_file_system.logs.file_system_id
+    validation = data.aws_efs_file_system.validation.file_system_id
+    output = data.aws_efs_file_system.output.file_system_id
+  }
+  environment = var.environment
+  iam_execution_role_arn = data.aws_iam_role.exe_role.arn
+  iam_job_role_arn = data.aws_iam_role.job_role.arn
+  prefix = var.prefix
 }
